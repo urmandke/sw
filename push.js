51af12d22d8160641e1e632908916160fda6bc34
Udayraj Mandke <urmandke@gmail.com>
2020-12-02 15:51:22 +0530

    fix

--- push.js
+++ push.js
@@ -5,8 +5,8 @@ let vapidKeys = {
     privateKey: 'r1o129oA-R2AQxT8oFgEYSUFExCmAK9b9wPp6RRKk2Q'
 }
 
-push.setVapidDetails('mailto:test@coder.co.uk',vapidKeys.publicKey,vapid.privateKey)
+push.setVapidDetails('mailto:test@coder.co.uk',vapidKeys.publicKey,vapidKeys.privateKey)
 
-let sub = {};
+let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/d4G114_4yhY:APA91bFEWrjje0pkmtBoCORXAf6V8tMueQ699_qkBxvMuoSAVSaM_03wpK6-mYjCiHj72eSkSyPbwvcSBuXkY0E4d5RBQk08QJirkUDSBs0_ree8XkplqP_p_ViwfuP6DDAueK-E3K1f","expirationTime":null,"keys":{"p256dh":"BND_QELGCB812ItWlmrxyUMAK-ep80SACbBSi2bSFkXzktBxTMoUYymN7ENOKXDXiRh9uVoMkDQyOz4FkuudIWA","auth":"IetKfPSxyAbbdlZm-J4eOA"}};
 
 push.sendNotification(sub, 'Test Message') 
--- sw.js
+++ sw.js
@@ -1,3 +1,19 @@
 self.addEventListener('push', () =>{
-    self.registration.sendNotification('test mesage', {});
-})
+    var options = {
+        body: 'This notification was generated from a push',
+        icon: 'images/example.png',
+        vibrate: [100, 50, 100],
+        data: {
+            dateOfArrival: Date.now(),
+            primaryKey: '2'
+        },
+        actions: [
+            {action: 'explore', title: 'Explore this new world',
+                icon: 'images/checkmark.png'},
+            {action: 'close', title: 'Close',
+                icon: 'images/xmark.png'},
+        ]
+    };
+    e.waitUntil(
+        self.registration.showNotification('test mesage', {});
+});
